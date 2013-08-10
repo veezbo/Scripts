1b349747b04bdfef6d1f51206bd94edf6e33ac92
Vibhor Kumar <vibhor@caltech.edu>
2013-08-09 22:40:56 -0700

    push

--- get_file_extension.sh
+++ get_file_extension.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+#Grab the Full File Name
 full_file="$1"
+
+
 file_name=$(basename $full_file)
 extension="${file_name##*.}"
+
+#Output extension
 echo "Extension:" $extension
+
+
 file_name="${file_name%.*}"
+
+#Output filename (without extension)
 echo "File Name:" $file_name
