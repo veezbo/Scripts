931ea5e8dcfd487f2af4891c9d2571477ca14b00
= <vibhor@caltech.edu>
2013-09-11 15:11:39 -0700

    can apply git commands to multiple git repositories

---
+++ gits.py
@@ -0,0 +1,41 @@
+import os, sys, subprocess
+import argparse
+from argparse import RawTextHelpFormatter
+
+
+parser = argparse.ArgumentParser(description="Description: \n\tApplies an action to all git repos in a certain directory", epilog="Example Usage: \n\npython recursive_git.py status\npython recursive_git diff", formatter_class=RawTextHelpFormatter) 
+parser.add_argument("command", help="OPTIONAL: git command to execute; default is status", nargs='?', default="status")
+parser.add_argument("arguments", help="OPTIONAL: git command's arguments", nargs="*")
+parser.add_argument("-d", "--dirs", help="specific directories to run command in", nargs="*")
+
+
+#Parse arguments
+args = parser.parse_args()
+
+#Determines if a directory is a git repository
+def isGitRepo(dir):
+	return '.git' in os.walk(dir).next()[1]
+
+
+dirs=[]
+
+if args.dirs:
+	#Get all Git Repositories from Given List
+	dirs = filter(isGitRepo, args.dirs)
+
+else:
+	#Get all Git Repositories in Current Folder
+	dirs = filter(isGitRepo, os.walk('.').next()[1])
+
+
+#Make the call for each
+for dir in dirs:
+	os.chdir(dir)
+	print "\nDIRECTORY: " + dir
+	r = subprocess.call(['git', args.command] + args.arguments)
+	os.chdir('..')
+
+
+#Finish
+print "\nSUCCESSFULLY FINISHED\n"
+sys.exit(0)
