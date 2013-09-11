d3dc27d4d7eb134b4a5b533985dc88a28c98a6d5
= <vibhor@caltech.edu>
2013-09-11 15:11:02 -0700

    template .py script file

---
+++ template.py
@@ -0,0 +1,68 @@
+import os, sys, subprocess, shutil
+import argparse
+from argparse import RawTextHelpFormatter
+
+#Prints help message and quits
+def help(parser):
+	print ""
+	parser.print_help()
+	print ""
+	sys.exit(1)
+
+
+#Prints error and quits
+def error(msg, parser):
+	print "ERROR: " + msg
+	help(parser)
+
+
+#Useful finish method
+def finish():
+	print "\nSUCCESSFULLY FINISHED\n"
+	sys.exit(0)
+
+
+#Useful methods (to keep in mind)
+#	map(function, list)
+#	filter(function, list)
+
+
+
+#Useful Wrapper terminal commands
+def cd(path):
+	os.chdir(path)
+def ls(path='.'):
+	return os.listdir(path)
+def mkdir(path):
+	os.mkdir(path)
+def rm(path):
+	os.remove(path)
+def rmr(path):
+	os.rmdir(path)
+def mv(src, dest):
+	shutil.move(src, dest)
+def cp(src, dest):
+	shutil.copy(src, dest)
+def pwd():
+	return os.getcwd()
+def chmod(path, mode):
+	os.chmod(path, mode)
+
+
+parser = argparse.ArgumentParser(description="Description: ", epilog="********Example Usages*********: \n\n", formatter_class=RawTextHelpFormatter)
+parser._optionals.title = "Flag Arguments"
+
+#Add more arguments here
+
+# #Print help if no arguments given (use if required arguments)
+# if len(sys.argv) == 1:
+# 	print ""
+# 	parser.print_help()
+# 	print ""
+# 	sys.exit(1)
+
+args = parser.parse_args()
+
+
+
+finish()
