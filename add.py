9ef129c433fda2bca110fbbfaf230803f2132e27
Vibhor Kumar <vibhor@caltech.edu>
2013-09-12 18:50:29 -0700

    extended template with example argument, and new python adder script

--- add.py
+++ add.py
@@ -53,21 +53,31 @@ def chmod(path, mode):
 	os.chmod(path, mode)
 
 
-parser = argparse.ArgumentParser(description="Description: ", epilog="********Example Usages*********: \n\n", formatter_class=RawTextHelpFormatter)
+parser = argparse.ArgumentParser(description="Description: Adds urls to the file Batch_Image_Downloader reads from", epilog="********Example Usages*********:\n\npython add.py <link>\n\npython urls_to_download <link> <link>", formatter_class=RawTextHelpFormatter)
 parser._optionals.title = "Flag Arguments"
-parser.add_argument("argument", help="OPTIONAL/REQUIRED: help message")
-#Add more arguments here
+parser.add_argument("file", help="OPTIONAL: the file name that want to add to", nargs="?", default="urls")
+parser.add_argument("urls", help="REQUIRED: enter the url that you want to add to the file", nargs="+")
 
-# #Print help if no arguments given (use if required arguments)
-# if len(sys.argv) == 1:
-# 	print ""
-# 	parser.print_help()
-# 	print ""
-# 	sys.exit(1)
+#Print help if no arguments given (use if required arguments)
+if len(sys.argv) == 1:
+	print ""
+	parser.print_help()
+	print ""
+	sys.exit(1)
 
-#Parse Arguments
 args = parser.parse_args()
 
+
+#Check if input file exists
+if not os.exists(args.file):
+	error("Input file (default is urls) does not exist")
+
+
+#Append the urls to the file
+for url in args.urls:
+	f = subprocess.open(args.file)
+	f.append(url+'/n')
+f.close()
 
 
 finish()
